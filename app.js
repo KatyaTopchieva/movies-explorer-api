8a4b7f8c3d3f202604aa82900469726cba4abca9
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-10-24 17:13:22 +0300

    "feat: update routing"

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+
+const users = require('./routes/users');
+const movies = require('./routes/movies');
+const pathNotFound = require('./routes/not-found');
+
+const { createUser, login } = require('./controllers/users');
+const { signUp, signIn } = require('./utils/validations');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.post('/signin', signIn, login);
+app.post('/signup', signUp, createUser);
+app.use(auth);
+
+app.use(movies);
+app.use(users);
+app.use(pathNotFound);
+
 mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 });
+
+app.use(errors());
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
