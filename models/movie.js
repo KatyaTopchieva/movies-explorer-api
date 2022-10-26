277af2c10ef8daff720c417531e261a5f238b3d5
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-10-26 16:37:47 +0300

    "fix: update schema movies"

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,4 @@
 /* eslint linebreak-style: 0 */
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 
@@ -66,24 +65,5 @@ const movieSchema = new mongoose.Schema({
     required: true,
   },
 });
-
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
-          }
-
-          return user;
-        });
-    });
-};
-
 
 module.exports = mongoose.model('movie', movieSchema);
